271cddab1d4ae2f406f4ed1dc4ecfae92502679f
JuanZea <61364624+JuanZea@users.noreply.github.com>
2021-01-17 19:19:00 -0500

    feat: Funcionalidad completa para S1 con paréntesis incluidos

--- Practica2TL/src/Models/Grammar.java
+++ Practica2TL/src/Models/Grammar.java
@@ -5,12 +5,24 @@ import java.util.Arrays;
 
 public class Grammar {
 
+    int parenthesis;
     private ArrayList<Character> alphabet;
     private ArrayList<String> reservedWords;
 
+    // iIndicadores de la gramática principal
     private boolean ifMode;
     private boolean whileMode;
-    private boolean error;
+    private boolean exArit;
+    private boolean exLog;
+    private boolean exChar;
+
+    // iIndicadores de la gramática de expresiones aritméticas
+    private boolean exArit_e1;
+    private boolean exArit_e2;
+    private boolean exArit_e3;
+    private boolean exArit_e4;
+
+    private String error;
 
     public Grammar() {
         // Create Alphabet
@@ -29,7 +41,7 @@ public class Grammar {
         String response;
 
         // Se usan las producciones del símbolo inicial de la gramática
-        response = this.S1(line);
+        response = this.S_S1(line);
         if (!(this.ifMode || this.whileMode)) {
             return response;
         }
@@ -38,54 +50,58 @@ public class Grammar {
     }
 
     public void initialize() {
+        this.parenthesis = 0;
         this.ifMode = false;
         this.whileMode = false;
-        this.error = false;
+        exArit = false;
+        exLog = false;
+        exChar = false;
+        this.error = null;
     }
 
     /**
      * Producción #1: <S> --> <ESP><TIPO> <ESP><NV><ESP>=<ESP><EX><ESP>;<ESP>
      */
-    private String S1(String line) {
+    private String S_S1(String line) {
         // <ESP>
         line = this.ESP(line);
         // <TIPO>
         line = this.TYPE(line);
-        if (this.hasError(line)) {
-            return this.translation(line);
+        if (this.error != null) { // Filtro de error
+            return this.translation(this.error);
         }
         // " "
         line = this.terminal(line , ' ');
-        if (this.hasError(line)) {
-            return this.translation(line);
+        if (this.error != null) { // Filtro de error
+            return this.translation(this.error);
         }
         // <ESP>
         line = this.ESP(line);
         // <NV>
         line = this.NV(line);
-        if (this.hasError(line)) {
-            return this.translation(line);
+        if (this.error != null) { // Filtro de error
+            return this.translation(this.error);
         }
         // <ESP>
         line = this.ESP(line);
         // "="
         line = this.terminal(line , '=');
-        if (this.hasError(line)) {
-            return this.translation(line);
+        if (this.error != null) { // Filtro de error
+            return this.translation(this.error);
         }
         // <ESP>
         line = this.ESP(line);
         // <EX>
         line = this.EX(line);
-        if (this.hasError(line)) {
-            return this.translation(line);
+        if (this.error != null) { // Filtro de error
+            return this.translation(this.error);
         }
         // <ESP>
         line = this.ESP(line);
         // ";"
         line = this.terminal(line , ';');
-        if (this.hasError(line)) {
-            return this.translation(line);
+        if (this.error != null) { // Filtro de error
+            return this.translation(this.error);
         }
         // <ESP>
         line = this.ESP(line);
@@ -116,21 +132,20 @@ public class Grammar {
     public String TYPE(String line) {
         // Verificación de la linea
         if (line.length() == 0) {
-            this.error = true;
-            return "E1";
+            return this.error = "E1";
         }
 
         // Lectura de tipos de dato
-        if (line.startsWith("int")) {
+        if (line.startsWith("int ")) {
             return line.substring(3);
         }
-        if (line.startsWith("char")) {
+        if (line.startsWith("char ")) {
             return line.substring(4);
         }
-        if (line.startsWith("double")) {
+        if (line.startsWith("double ")) {
             return line.substring(6);
         }
-        if (line.startsWith("boolean")) {
+        if (line.startsWith("boolean ")) {
             return line.substring(7);
         }
         return " ".concat(line);
@@ -139,8 +154,7 @@ public class Grammar {
     public String NV(String line) {
         // Verificación de la linea
         if (line.length() == 0) {
-            this.error = true;
-            return "E2";
+            return this.error = "E2";
         }
 
         String word = null;
@@ -151,11 +165,14 @@ public class Grammar {
             }
         }
         if (word == null) {
-            this.error = true;
             if (this.varName(line)) {
-                return "E=";
+                // Se excluyen las palabras reservadas
+                if (this.reservedWords.contains(line)) {
+                    return this.error = "E5";
+                }
+                return this.error = "E=";
             }
-            return "E3";
+            return this.error = "E3";
         }
 
         // Se excluyen las palabras reservadas
@@ -166,50 +183,170 @@ public class Grammar {
             if (word.equals("while")) {
                 this.whileMode = true;
             }
-            this.error = true;
-            return "E5";
+            return this.error = "E5";
         }
         if (this.varName(word)) {
             return line.substring(word.length());
         }
-        this.error = true;
-        return "E3";
+        return this.error = "E3";
     }
 
     public String EX(String line) {
         // Verificación de la linea
         if (line.length() == 0) {
-            this.error = true;
-            return "E4";
+            return this.error = "E4";
         }
 
-        if (line.startsWith("EX")) {
-            return line.substring(2);
+        // Verificación de inicio de expresión
+        if (!this.alphabet.contains(line.charAt(0)) && line.charAt(0) != '(') {
+            return this.error = "E4";
         }
-        this.error = true;
-        return "Ez";
+
+        // Arrange
+        boolean numRead = false;
+        boolean opdorRead = false;
+        String word = null;
+        int index = 0;
+
+        // Se guardan los paréntesis
+        while (index < line.length()) {
+            if (line.charAt(index) != '(') {
+                if (line.charAt(index) == ' ') {
+                    index++;
+                    continue;
+                }
+                break;
+            }
+            this.parenthesis++;
+            index++;
+        }
+        line = line.substring(index);
+        index = 0;
+
+        // Se intenta leer un número
+        while (index < line.length()) {
+            if (!this.isNumber(line.charAt(index))) {
+                break;
+            }
+            numRead = true;
+            index++;
+        }
+        if (numRead) {
+            line = line.substring(index);
+        }
+        index = 0;
+
+        // Verificación de la linea
+        if (line.length() == 0 && numRead) {
+            return this.error = "E;";
+        }
+
+        // Se deshacen los paréntesis de cierre
+        if (numRead) {
+            while (index < line.length()) {
+                if (line.charAt(index) != ')') {
+                    if (line.charAt(index) == ' ') {
+                        index++;
+                        continue;
+                    }
+                    break;
+                }
+                this.parenthesis--;
+                index++;
+            }
+            if (parenthesis < 0) {
+                return this.error = "E7";
+            }
+            line = line.substring(index);
+            index = 0;
+        }
+
+        // Se intenta leer un nombre de variable teniendo en cuenta que no se leyó un número
+        if (!numRead) {
+            while (index < line.length()) {
+                if (!this.alphabet.contains(line.charAt(index))) {
+                    word = line.substring(0, index);
+                    break;
+                }
+                index++;
+            }
+            index = 0;
+            if (word == null) {
+                if (this.varName(line)) {
+                    // Se excluyen las palabras reservadas
+                    if (this.reservedWords.contains(line)) {
+                        return this.error = "E5";
+                    }
+                    return this.error = "E;";
+                }
+                return this.error = "E3";
+            }
+            // Se excluyen las palabras reservadas
+            if (this.reservedWords.contains(word)) {
+                return this.error = "E5";
+            }
+            if (this.varName(word)) {
+                line = line.substring(word.length());
+            }
+        }
+
+        // Verificación de la linea
+        if (line.length() == 0) {
+            return "";
+        }
+
+        // Se deshacen los paréntesis de cierre
+        while (index < line.length()) {
+            if (line.charAt(index) != ')') {
+                if (line.charAt(index) == ' ') {
+                    index++;
+                    continue;
+                }
+                break;
+            }
+            this.parenthesis--;
+            index++;
+        }
+        if (parenthesis < 0) {
+            return this.error = "E7";
+        }
+        line = line.substring(index);
+
+        // <ESP>
+        line = this.ESP(line);
+
+        // Se intenta leer in operador
+        if (line.startsWith("==") || line.startsWith("!=") || line.startsWith("&&") || line.startsWith("||") || line.startsWith("<=") || line.startsWith(">=")) {
+            line = line.substring(2);
+            opdorRead = true;
+        } else if (line.startsWith("+") || line.startsWith("-") || line.startsWith("*") || line.startsWith("/") || line.startsWith("%") || line.startsWith("&") || line.startsWith("|") || line.startsWith("<") || line.startsWith(">")) {
+            line = line.substring(1);
+            opdorRead = true;
+        }
+
+
+        // <ESP>
+        line = this.ESP(line);
+
+        // Se hace llamado recursivo a leer una expresión
+        if (opdorRead) {
+            return this.EX(line);
+        }
+
+        // Finaliza la lectura correctamente
+        return line;
     }
 
     public String terminal(String line, char terminal) {
         // Verificación de la linea
         if (line.length() == 0) {
-            this.error = true;
-            return "E".concat(String.valueOf(terminal));
+            return this.error = "E".concat(String.valueOf(terminal));
         }
 
         if (line.charAt(0) == terminal) {
             return line.substring(1);
         }
-        this.error = true;
-        return "E".concat(String.valueOf(terminal));
-    }
-
-    public boolean hasError(String line) {
-        // Verificación de la linea
-        if (line.length() == 0) {
-            return false;
-        }
-        return line.charAt(0) == 'E' && this.error;
+        return this.error = "E".concat(String.valueOf(terminal));
     }
 
     public boolean varName(String line) {
@@ -273,6 +410,12 @@ public class Grammar {
             }
             case 5: {
                 return "uso de palabra reservada";
+            }
+            case 6: {
+                return "se esperaba un operador";
+            }
+            case 7: {
+                return "la expresión no esta balanceada con respecto a sus paréntesis";
             }
         }
     }
